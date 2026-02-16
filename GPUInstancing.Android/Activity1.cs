6fb1aaaf60ea94f7910a03a3fb4608ea83f82a10
ryancheung <ryancheung.go@gmail.com>
2026-02-16 11:00:14 +0800

    Updated to latest fna.net

--- GPUInstancing.Android/Activity1.cs
+++ GPUInstancing.Android/Activity1.cs
@@ -3,6 +3,9 @@ using Android.Content.PM;
 using Android.OS;
 using Android.Views;
 using Microsoft.Xna.Framework;
+using Org.Libsdl.App;
+using SDL = SDL3.SDL;
+using System;
 
 namespace GPUInstancing.Android
 {
@@ -15,13 +18,39 @@ namespace GPUInstancing.Android
         ScreenOrientation = ScreenOrientation.SensorLandscape,
         ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout
     )]
-    public class Activity1 : AndroidGameActivityEXT
+    public class Activity1 : SDLActivity
     {
-        protected override void SDLMain()
+        protected override string[] GetLibraries()
         {
-            using (var game = new Game1())
-                game.Run();
+            return ["SDL3"];
         }
+
+        protected override void Main()
+        {
+            // Enable high DPI "Retina" support. Trust us, you'll want this.
+            SDL.SDL_SetHint("FNA_GRAPHICS_ENABLE_HIGHDPI", "1");
+
+            // Keep mouse and touch input separate.
+            SDL.SDL_SetHint(SDL.SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
+            SDL.SDL_SetHint(SDL.SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
+            SDL.SDL_SetHint(SDL.SDL_HINT_PEN_TOUCH_EVENTS, "0");
+            SDL.SDL_SetHint("FNA3D_FORCE_DRIVER", "Vulkan");
+            SDL.SDL_SetHint("FNA3D_OPENGL_FORCE_ES3", "1");
+
+            SDL.SDL_RunApp(0, IntPtr.Zero, FakeMain, IntPtr.Zero);
+        }
+
+        static int FakeMain(int argc, IntPtr argv)
+        {
+            RealMain();
+            return 0;
+        }
+
+		static void RealMain()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
 
         public override void OnWindowFocusChanged(bool hasFocus)
         {
